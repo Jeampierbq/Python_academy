8ba836de5ed40816d1259c23be8a4866906f11fe
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-04-26 13:02:37 -0500

    operadores2

---
+++ Leccion02/Operadores02.py
@@ -0,0 +1,16 @@
+#Operadores de asignacion
+
+miVariable = 10
+print(miVariable)
+
+#Incremento de asignacion
+miVariable += 1
+print(miVariable)
+
+#Multiplicacion
+miVariable *= 10
+print(miVariable)
+
+#Division
+miVariable /=2
+print(miVariable)
