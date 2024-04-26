f0e44247d2c63b28e9fecc39fcaafdc1d53da5d5
Jeampierbq <Jeampierbarrios04@gmail.com>
2024-04-26 03:15:32 -0500

    Agregado session2

---
+++ Leccion02/Operadores01.py
@@ -0,0 +1,11 @@
+#Calcular area y perimetro de un rectangulo
+
+alto = int(input("Proporciona el alto del rectangulo :"))
+ancho = int(input("Proporciona el ancho del rectangulo :"))
+
+area = alto * ancho
+perimetro = (alto + ancho)*2
+
+print("Area :", area)
+print("Perimetro", perimetro )
+
